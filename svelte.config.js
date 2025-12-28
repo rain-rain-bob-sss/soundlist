c1a60abb3e98611081e949eafea9485ddb89bb30
[TW]Rain_Bob <61649368+rain-rain-bob-sss@users.noreply.github.com>
2025-12-28 16:46:30 +0800

    forgot

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,18 @@
-import adapter from '@sveltejs/adapter-cloudflare';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+    preprocess: vitePreprocess(),
 
-	kit: {
-		adapter: adapter()
-	}
+    kit: {
+         adapter: adapter({
+            fallback: '404.html'
+         }),
+         paths: {
+            base: '/fundamental_svelte'
+         }
+    }
 };
 
 export default config;
